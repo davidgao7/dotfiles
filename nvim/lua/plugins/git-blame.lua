e8d8644e50a06900d53d439125f10e9df1747f09
David Gao <davidgao313@outlook.com>
2025-04-21 20:04:17 -0400

    find who did it first

--- nvim/lua/plugins/git-blame.lua
+++ nvim/lua/plugins/git-blame.lua
@@ -11,7 +11,7 @@ return {
         -- or lazy = false. One of both options will work.
         opts = {
             enabled = true,
-            message_template = " <summary> • <date> • <author> • <<sha>>",
+            message_template = " <author> • <date> • <summary> • <<sha>>",
             date_format = "%m-%d-%Y %H:%M:%S",
             virtual_text_column = 1,
         },
