5947ef665c2964bd1a6a8c1de5d19c3ba235ae31
David Gao <davidgao313@outlook.com>
2025-04-29 06:51:46 -0400

    dynanmic input showing

--- sketchybar/plugins/get_input_source.sh
+++ sketchybar/plugins/get_input_source.sh
@@ -1,13 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-# hangul and english item
+plist_data=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources 2>/dev/null)
 
-# Read the plist data
-plist_data=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources)
-current_input_source=$(echo "$plist_data" | plutil -convert xml1 -o - - | grep -A1 'KeyboardLayout Name' | tail -n1 | cut -d '>' -f2 | cut -d '<' -f1)
+layout_name=$(echo "$plist_data" | grep '"KeyboardLayout Name"' | head -n1 | cut -d '"' -f4)
+input_mode=$(echo "$plist_data" | grep '"Input Mode"' | head -n1 | cut -d '"' -f4)
 
-if [ "$current_input_source" = "ABC" ]; then
-    sketchybar --set input_source icon="􀂕"
-else
-    sketchybar --set input_source icon="􀂩"
+label=""
+icon=""
+
+# First prioritize layout_name if exists
+if [ -n "$layout_name" ]; then
+    case "$layout_name" in
+    "U.S.")
+        label="EN"
+        icon="􀂕"
+        ;;
+    "USInternational-PC")
+        label="PC"
+        icon="􀂕"
+        ;;
+    *)
+        label="?"
+        icon="􀍶"
+        ;;
+    esac
 fi
+
+# If no layout_name matched, fallback to input_mode
+if [ -n "$input_mode" ]; then
+    case "$input_mode" in
+    "com.apple.inputmethod.TCIM.Pinyin")
+        label="繁"
+        icon="􀅈"
+        ;;
+    "com.apple.inputmethod.SCIM.ITABC")
+        label="简"
+        icon="􀅈"
+        ;;
+    *)
+        label="拼"
+        icon="􀍶"
+        ;;
+    esac
+fi
+
+sketchybar --set input_source icon="$icon" label="$label"
