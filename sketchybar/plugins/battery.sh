634a62ceab5b61b6fa34d606ab594a27dcb90850
David Gao <davidgao313@outlook.com>
2025-05-01 16:41:27 -0400

    battery, leave as it is

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -6,37 +6,37 @@ PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power')"
 
 if [ "$PERCENTAGE" = "" ]; then
-  exit 0
+    exit 0
 fi
 
 case ${PERCENTAGE} in
-  [8-9][0-9] | 100)
+[8-9][0-9] | 100)
     ICON="􀛨"
     ICON_COLOR=$BATTERY_1
     ;;
-  7[0-9])
+7[0-9])
     ICON="􀺸"
     ICON_COLOR=$BATTERY_2
     ;;
-  [4-6][0-9])
+[4-6][0-9])
     ICON="􀺶"
     ICON_COLOR=$BATTERY_3
     ;;
-  [1-3][0-9])
-      ICON="􀛩"
+[1-3][0-9])
+    ICON="􀛩"
     ICON_COLOR=$BATTERY_4
     ;;
-  [0-9])
+[0-9])
     ICON="􀛪"
     ICON_COLOR=$BATTERY_5
     ;;
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-  ICON="􀢋"
-  ICON_COLOR=$YELLOW
+    ICON="􀢋"
+    ICON_COLOR=$YELLOW
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%" icon.color=${ICON_COLOR}
+sketchybar --set "$NAME" icon="$ICON" icon.color=${ICON_COLOR} label="${PERCENTAGE}%"
