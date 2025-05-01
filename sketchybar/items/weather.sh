8309e4c0336596ab9b228f9475602a035f2a0649
David Gao <davidgao313@outlook.com>
2025-05-01 03:47:14 -0400

    weather

--- sketchybar/items/weather.sh
+++ sketchybar/items/weather.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-sketchybar --add item weather right \
-  --set weather \
-  icon=󰖐 \
-  script="$PLUGIN_DIR/weather.sh" \
-  update_freq=1500 \
-  --subscribe weather mouse.clicked
-
+sketchybar --add item weather left \
+    --set weather \
+    icon=󰖐 \
+    script="$PLUGIN_DIR/weather.sh" \
+    update_freq=1500 \
+    --subscribe weather mouse.clicked
