ddb54508e41aba35ae4ba314ec77102249491602
David Gao <davidgao313@outlook.com>
2025-05-02 21:19:03 -0400

    true zen mode, only space + zen trigger displaying

--- sketchybar/plugins/zen.sh
+++ sketchybar/plugins/zen.sh
@@ -2,27 +2,42 @@
 
 ZEN_FLAG="/tmp/sketchybar_zen_mode"
 
+HIDE_ITEMS=(
+    front_app
+    disk
+    ram
+    cpu.percent
+    net.up
+    net.down
+    brew
+    calendar
+    wifi
+    volume
+    volume_icon
+    battery
+    input_source
+    media
+    weather
+    status
+)
+
 if [[ -f "$ZEN_FLAG" ]]; then
     # 🔓 Turn OFF Zen Mode
     rm "$ZEN_FLAG"
 
-    sketchybar --set front_app drawing=on \
-        --set network_rates drawing=on \
-        --set wifi drawing=on \
-        --set calendar drawing=on \
-        --set brew drawing=on \
-        --set media drawing=on \
-        --set zen icon="🧘" label="Zen"
+    for item in "${HIDE_ITEMS[@]}"; do
+        sketchybar --set "$item" drawing=on
+    done
+
+    sketchybar --set zen icon="🧘" label="Zen"
 
 else
     # 🔒 Turn ON Zen Mode
     touch "$ZEN_FLAG"
 
-    sketchybar --set front_app drawing=off \
-        --set network_rates drawing=off \
-        --set wifi drawing=off \
-        --set calendar drawing=off \
-        --set brew drawing=off \
-        --set media drawing=off \
-        --set zen icon="🌙" label="Focus"
+    for item in "${HIDE_ITEMS[@]}"; do
+        sketchybar --set "$item" drawing=off
+    done
+
+    sketchybar --set zen icon="🌙" label="Focus"
 fi
