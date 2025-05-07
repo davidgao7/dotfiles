f4881b403ef7bbbf669049f1c42ffcb11132cb58
David Gao <davidgao313@outlook.com>
2025-05-07 07:24:50 -0400

    keymaps to toggle virtual text for diagnostic
    new feature in nvim, but will cause line shift, I like my floating
    window solution better

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1002,6 +1002,10 @@ return {
         })
       end,
     },
+    vim.keymap.set("n", "gK", function()
+      local new_config = not vim.diagnostic.config().virtual_lines
+      vim.diagnostic.config({ virtual_lines = new_config })
+    end, { desc = "Toggle diagnostic virtual_lines" }),
   },
 
   -- autopair
