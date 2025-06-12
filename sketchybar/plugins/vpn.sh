4e84dcebac6f1c0b43a4143df72af2e83d8e15d3
David Gao <davidgao313@outlook.com>
2025-06-12 14:09:00 +0800

    grep -q has no output(since quietly), some vpn are not using utun[0-9]
    utun = common macOS VPN tunnel
    
    ipsec = IPsec VPN
    
    ppp / pppoe = old-style dial-up and some VPNs
    
    vpn = just in case any interface is labeled that way

--- sketchybar/plugins/vpn.sh
+++ sketchybar/plugins/vpn.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-VPN_STATUS=$(scutil --nwi | grep -qE 'utun[0-9]')
-
-if scutil --nwi | grep -qE 'utun[0-9]'; then
+if scutil --nwi | grep -qE '^\s*(utun|ipsec|ppp|pppoe|vpn)[0-9]*'; then
     # VPN is up
     sketchybar --set "$NAME" \
         icon="􀙨" \
