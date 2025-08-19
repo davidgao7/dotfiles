cd56bcf7d8df8ed2fe239540adfe060c87053307
David Gao <davidgao313@outlook.com>
2025-08-20 04:02:10 +0800

    add debug log

--- hypr/random-wallpaper.sh
+++ hypr/random-wallpaper.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-# Define the directory where your wallpapers are stored
+LOG_FILE="/tmp/swww_log.txt"
+echo "--- Starting wallpaper script ---" >"$LOG_FILE"
+echo "Attempting to change wallpaper..." >>"$LOG_FILE"
+
 WALLPAPER_DIR="$HOME/anime/"
+echo "Wallpaper directory: $WALLPAPER_DIR" >>"$LOG_FILE"
 
-# Find all image files in the directory
-WALLPAPERS=($(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" -o -name "*.svg" -o -name "*.webp" -o -name "*.bmp" \)))
+WALLPAPERS=($(/usr/bin/find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.gif" -o -name "*.svg" -o -name "*.webp" -o -name "*.bmp" \)))
 
-# Check if any wallpapers were found
 if [ ${#WALLPAPERS[@]} -eq 0 ]; then
-    echo "No wallpapers found in $WALLPAPER_DIR"
+    echo "Error: No wallpapers found." >>"$LOG_FILE"
     exit 1
 fi
 
-# Select a random wallpaper from the array
 RANDOM_WALLPAPER="${WALLPAPERS[$RANDOM % ${#WALLPAPERS[@]}]}"
+echo "Selected wallpaper: $RANDOM_WALLPAPER" >>"$LOG_FILE"
 
-# Define the available transition types
 TRANSITIONS=("simple" "left" "right" "top" "bottom" "center" "outer" "any" "random")
-
-# Select a random transition type
 RANDOM_TRANSITION="${TRANSITIONS[$RANDOM % ${#TRANSITIONS[@]}]}"
+echo "Selected transition: $RANDOM_TRANSITION" >>"$LOG_FILE"
 
-# Set the wallpaper with a random transition type using swww
-swww img "$RANDOM_WALLPAPER" --resize="fit" --transition-type "$RANDOM_TRANSITION"
+/usr/bin/swww img "$RANDOM_WALLPAPER" --transition-type "$RANDOM_TRANSITION"
 
-echo "Changed wallpaper to $RANDOM_WALLPAPER with transition type: $RANDOM_TRANSITION"
+echo "Finished script." >>"$LOG_FILE"
