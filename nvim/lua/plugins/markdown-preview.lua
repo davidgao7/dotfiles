ad18cb992a3a519b10ea457daded9b87246e80d0
David Gao <davidgao313@outlook.com>
2025-10-10 16:43:43 +0800

    markdown-preview scroll center

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,22 +1,24 @@
 return {
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = function()
-            require("lazy").load({ plugins = { "markdown-preview.nvim" } })
-            vim.fn["mkdp#util#install"]()
-        end,
-        keys = {
-            {
-                "<leader>mt",
-                ft = "markdown",
-                "<cmd>MarkdownPreviewToggle<cr>",
-                desc = "Markdown Preview",
-            },
-        },
-        config = function()
-            vim.cmd([[do FileType]])
-        end,
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>mt",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
     },
-
+    options = {
+      sync_scroll_type = "middle",
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
 }
