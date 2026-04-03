1f4626f86378f1e6caf00aa75333af9b0a948eb7
David Gao <davidgao313@outlook.com>
2026-04-03 10:19:43 +0800

    feat: add lazygit config path
    Signed-off-by: David Gao <davidgao313@outlook.com>

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -1,24 +1,37 @@
 return {
-    -- nvim v0.8.0
-    "kdheepak/lazygit.nvim",
-    lazy = false,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim",
-    },
-    config = function()
-        require("telescope").load_extension("lazygit")
-        vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
-        vim.keymap.set("n", "<leader>gt", "<cmd>LazyGitConfig<cr>", { desc = "LazyGitConfig" })
-        vim.keymap.set("n", "<leader>gf", "<cmd>LazyGitCurrentFile<cr>", { desc = "LazyGitCurrentFile" })
-        vim.keymap.set("n", "<leader>gv", "<cmd>LazyGitFilterCurrentFile<cr>", { desc = "LazyGitFilterCurrentFile" })
-    end,
+  -- nvim v0.8.0
+  "kdheepak/lazygit.nvim",
+  lazy = false,
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    lazygit_use_custom_config_file_path = "~/.config/lazygit/config.yml",
+  },
+  config = function()
+    require("telescope").load_extension("lazygit")
+    vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+    vim.keymap.set("n", "<leader>gt", "<cmd>LazyGitConfig<cr>", { desc = "LazyGitConfig" })
+    vim.keymap.set(
+      "n",
+      "<leader>gf",
+      "<cmd>LazyGitCurrentFile<cr>",
+      { desc = "LazyGitCurrentFile" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>gv",
+      "<cmd>LazyGitFilterCurrentFile<cr>",
+      { desc = "LazyGitFilterCurrentFile" }
+    )
+  end,
 }
