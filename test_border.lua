8d332cb7563744bddb2ba4bcde80d04bf29ec784
David Gao <davidgao313@outlook.com>
2026-04-18 21:36:30 +0800

    chore: remove test files
    Signed-off-by: David Gao <davidgao313@outlook.com>

--- test_border.lua
+++
@@ -1,4 +0,0 @@
-local bufnr = vim.api.nvim_create_buf(false, true)
-local ui = { border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }, title = " ฅ^•ﻌ•^ฅ " }
-local win = vim.api.nvim_open_win(bufnr, false, {relative="cursor", row=1, col=1, width=10, height=10, border=ui.border, title=ui.title})
-print("Win created:", win)
--- test_title2.lua
+++
@@ -1,3 +0,0 @@
-local ui = { border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }, title = "title" }
-local b, w = vim.lsp.util.open_floating_preview({"test"}, "markdown", ui)
-print("Title:", vim.inspect(vim.api.nvim_win_get_config(w).title))
